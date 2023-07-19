83c5d02cd7b37cbaeb99e700461585fbaa4b39e3
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-07-19 12:37:45 +0300

    created mini calculator

--- src/com/prog/Main.java
+++ src/com/prog/Main.java
@@ -5,8 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите данные: ");
-        String data = input.nextLine();
-        System.out.print("\n Результат: " + data);
+
+        System.out.print("Введите первое число: ");
+        float num1 = input.nextFloat();
+
+        System.out.print("Введите второе число: ");
+        float num2 = input.nextFloat();
+
+        float res1 = num1 + num2;
+        float res2 = num1 - num2;
+        float res3 = num1 * num2;
+        float res4 = num1 / num2;
+        float res5 = num1 % num2;
+
+        System.out.println("Результат: ");
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 + "\n" + res5);
     }
+
 }
