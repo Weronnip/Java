ff9a6cd1bafc713807a0c5a522375cc6dce1baf8
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-08-02 13:38:28 +0800

    create class function.java, return data in class function
    add code, work with array, Collection array

--- src/com/prog/array.java
+++ src/com/prog/array.java
@@ -5,38 +5,89 @@ import java.util.Scanner;
 public class array {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Введите номер: ");
-        int arrays = input.nextInt();
 
         int[] array = new int[5];
-        array[0] = 12;
-        array[1] = 24;
-        array[2] = 48;
-        array[3] = 96;
-        array[4] = 192;
 
+        Scanner scan = new Scanner(System.in);
 
-        switch (arrays) {
-            case 0:
-                System.out.print("Число: " + array[0]);
-                break;
-            case 1:
-                System.out.print("Число: " + array[1]);
-                break;
-            case 2:
-                System.out.print("Число: " + array[2]);
-                break;
-            case 3:
-                System.out.print("Число: " + array[3]);
-                break;
-            case 4:
-                System.out.print("Число: " + array[4]);
-                break;
-            default:
-                System.out.print("Такого номера не существует");
+        for(int i = 0; i < array.length; i++){
+            System.out.print("Enter element: ");
+            int value = scan.nextInt();
+            array[i] = value;
         }
 
+        int min = array[0];
+        for(int i = 0; i < array.length; i++){
+            if (array[i] < min)
+                min = array[i];
+        }
+        System.out.println("\nMinimal element: " + min);
+
+        int max = array[1];
+        for(int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Введите номер: ");
+//        int arrays = input.nextInt();
+//
+//        int[] array = new int[5];
+//        array[0] = 12;
+//        array[1] = 24;
+//        array[2] = 48;
+//        array[3] = 96;
+//        array[4] = 192;
+//
+//
+//        switch (arrays) {
+//            case 0:
+//                System.out.print("Число: " + array[0]);
+//                break;
+//            case 1:
+//                System.out.print("Число: " + array[1]);
+//                break;
+//            case 2:
+//                System.out.print("Число: " + array[2]);
+//                break;
+//            case 3:
+//                System.out.print("Число: " + array[3]);
+//                break;
+//            case 4:
+//                System.out.print("Число: " + array[4]);
+//                break;
+//            default:
+//                System.out.print("Такого номера не существует");
+//        }
+
+//        LinkedList<Float> number = new LinkedList<>();
+//        number.add(1.234f);
+//        number.add(23.5456f);
+//        number.add(2143.3454645f);
+//
+//        System.out.println("Array size: " + number.size());
+//        System.out.println("Get float number: " + number.get(1));
+//
+//        for(Float i : number) {
+//            System.out.println("Element: " + i);
+//        }
+//
+//
+//
+////        ArrayList<Integer> number = new ArrayList<>();
+////        number.add(5);
+////        number.add(6);
+////        number.add(1, 53);
+////
+////        System.out.println("Size ArrayList: " + number.size());
+////        System.out.println("Get index number: " + number.get(0));
+////
+////        System.out.println("Remove number: " + number.remove(1));
+////
+////        for(Integer el : number) {
+////            System.out.println("Element: " + el);
+////        }
     }
 }
--- src/com/prog/function.java
+++ src/com/prog/function.java
@@ -1,6 +1,6 @@
 package com.prog;
 
-public class Main {
+public class function {
     public static void main(String[] args) {
         byte[] array = new byte[] {1, 2, 3};
 
