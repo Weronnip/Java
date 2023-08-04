a8eaadb518bb602b5e146c45aa55ed2b4870afe2
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-08-04 21:58:01 +0800

    Created Panel Car CreateCar.java, mini update Object.java

---
+++ src/com/prog/CreateCar.java
@@ -0,0 +1,51 @@
+package com.prog;
+
+import java.util.Scanner;
+
+public class CreateCar extends Object {
+
+    public CreateCar(String NameCar, float SpeedCar, int WeightCar, String ColorCar, float FuelCar, byte CoordinateCar) {
+        super(NameCar, SpeedCar, WeightCar, ColorCar, FuelCar, new byte[]{CoordinateCar});
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("----Panel Create Car----\n");
+        CratedCars();
+
+    }
+
+    public static CreateCar CratedCars() {
+        //CreateCar
+        Scanner cars = new Scanner(System.in);
+
+        System.out.println("----Create Car----\n");
+
+        //NameCar
+        System.out.print("Enter name car: ");
+        String NameCar = cars.nextLine();
+
+        //ColorCar
+        System.out.print("Enter color car: ");
+        String ColorCar = cars.nextLine();
+
+        //SpeedCar
+        System.out.print("Enter speed car: ");
+        float SpeedCar = cars.nextFloat();
+
+        //WeightCar
+        System.out.print("Enter weight car: ");
+        int WeightCar = cars.nextInt();
+
+        //FuelCar
+        System.out.print("Enter fuel car: ");
+        float FuelCar = cars.nextFloat();
+
+        //CoordinateCar
+        System.out.print("Enter coordinate car: ");
+        byte CoordinateCar = cars.nextByte();
+
+
+        return new CreateCar(NameCar, SpeedCar, WeightCar, ColorCar, FuelCar, CoordinateCar);
+    }
+}
--- src/com/prog/Object.java
+++ src/com/prog/Object.java
@@ -8,21 +8,21 @@ public class Object {
     private float fuel;
     private String name;
 
-    public Object(String name, float speed, int weight, String color, float fuel, byte[] coordinate) {
+    protected Object(String name, float speed, int weight, String color, float fuel, byte[] coordinate) {
         System.out.println("Created Car: \n");
         setValues(name, speed, weight, color, fuel, coordinate);
 
         System.out.println(getValues());
     }
 
-    public Object(int weight, byte[] coordinate) {
+    protected Object(int weight, byte[] coordinate) {
         System.out.print("Created Truck: ");
         this.weight = weight;
         this.coordinate = coordinate;
         System.out.println(this.getValues());
     }
 
-    public void setValues(String name, float speed, int weight, String color, float fuel, byte[] coordinate) {
+    protected void setValues(String name, float speed, int weight, String color, float fuel, byte[] coordinate) {
         this.name = name;
         this.speed = speed;
         this.weight = weight;
@@ -31,15 +31,15 @@ public class Object {
         this.coordinate = coordinate;
     }
 
-    public String getValues() {
-        String info = "Name car: " + name + "\n\nSpeed: " +
-                speed + "\nWeight: " + weight +
-                "\nColor: " + color + "\nFuel: " + fuel + "\n\n";
+    protected String getValues() {
+        String info = "Name car: " + name + "\n\nSpeed: "
+                + speed +  " km/h" + "\nWeight: " + weight +
+                "\nColor: " + color + "\nFuel: " + fuel + " L" + "\n";
 
-        String infoCoordinates = "Coordinetes:\n";
+        String infoCoordinates = "Coordinetes: ";
 
         for(int i = 0; i < coordinate.length; i++) {
-            infoCoordinates += coordinate[i] + "\n";
+            infoCoordinates += coordinate[i] + ".";
         }
         return info + infoCoordinates;
     }
